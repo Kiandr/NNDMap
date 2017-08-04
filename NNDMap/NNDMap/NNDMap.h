f57088adaab59da41249c15e4651a849b8b00686
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-08-04 16:38:18 -0700

    init commit

---
+++ NNDMap/NNDMap/NNDMap.h
@@ -0,0 +1,19 @@
+//
+//  NNDMap.h
+//  NNDMap
+//
+//  Created by Kian Davoudi-Rad on 2017-08-04.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NNDMap.
+FOUNDATION_EXPORT double NNDMapVersionNumber;
+
+//! Project version string for NNDMap.
+FOUNDATION_EXPORT const unsigned char NNDMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NNDMap/PublicHeader.h>
+
+
